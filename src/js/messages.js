f06da8baa28dbdbc27b22f2c484ee0ba3713256c
Igor Ognichenko <ognichenko.igor@gmail.com>
2023-02-14 14:20:22 +0200

    WWT-1947: updated button text

--- src/js/messages.js
+++ src/js/messages.js
@@ -1,4 +1,4 @@
 export default {
-  btnLabelConnect: "Purchase with Blade",
+  btnLabelConnect: "Purchase NFT",
   popupMessage: "Click on Blade Wallet icon from your browser extensions panel and follow instructions."
 }
